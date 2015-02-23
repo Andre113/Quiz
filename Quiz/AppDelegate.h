07d8ad7863675f063746811e509c41157ce66307
Andre Lucas Ota <AndreLucas@MacBook-Pro-de-Andre-2.local>
2015-02-23 14:54:45 -0300

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Andre Lucas Ota on 23/02/15.
+//  Copyright (c) 2015 Andre Lucas Ota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Andre Lucas Ota on 23/02/15.
+//  Copyright (c) 2015 Andre Lucas Ota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
